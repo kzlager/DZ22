3a761f0e203face996019db7d335ccaa2b4519c4
kzlager <highcolour@yandex.ru>
2022-02-14 08:26:54 +0300

    done

--- Program.cs
+++ Program.cs
@@ -1,13 +1,13 @@
 ﻿// 22. Найти расстояние между точками в пространстве 2d/3d
 
 //
-double Distance2D(double x1, double y1. double x2, double y2)
+double Distance2D(double x1, double y1, double x2, double y2)
 {
     double result = Math.Sqrt(Math.Pow(x2 - x1, 2) + Mzth.Pow(y2 - y1, 2));
     return result;
 }
 
-double Distance3D(double x1, double y1. double z1, double x2, double y2, double z2)
+double Distance3D(double x1, double y1, double z1, double x2, double y2, double z2)
 {
     double result = Math.Sqrt(Math.Pow(x2 - x1, 2) + Mzth.Pow(y2 - y1, 2) + Math.Pow(z2 - z1, 2));
     return result;
