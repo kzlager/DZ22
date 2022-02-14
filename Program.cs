7b67cd9b90a2fd5a631dc6ac367efb8640c1a9c2
kzlager <highcolour@yandex.ru>
2022-02-14 08:09:42 +0300

    new

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿// 22. Найти расстояние между точками в пространстве 2d/3d
+
+//
+double Distance2D(double x1, double y1. double x2, double y2)
+{
+    double result = Math.Sqrt(Math.Pow(x2 - x1, 2) + Mzth.Pow(y2 - y1, 2));
+    return result;
+}
+
