6f1e414f4289e5410ef436e70598624af74b8f1e
kzlager <highcolour@yandex.ru>
2022-02-14 08:13:32 +0300

    new2

--- Program.cs
+++ Program.cs
@@ -7,3 +7,11 @@ double Distance2D(double x1, double y1. double x2, double y2)
     return result;
 }
 
+double Distance3D(double x1, double y1. double z1, double x2, double y2, double z2)
+{
+    double result = Math.Sqrt(Math.Pow(x2 - x1, 2) + Mzth.Pow(y2 - y1, 2) + Math.Pow(z2 - z1, 2));
+    return result;
+}
+
+Console.Write("Введите пространство для поиска расстояния между точками (2d или 3d): ");
+string choice = Console.ReadLine();
